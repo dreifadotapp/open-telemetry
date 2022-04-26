86c41fdb350b99771100071db0e1b6f93897a36c
Ian Morgan <ian.j.morgan@gmail.com>
2022-04-26 15:21:06 +0100

    added convenience helpers to OpenTelemetryContext

--- src/main/kotlin/dreifa/app/opentelemetry/Propogator.kt
+++ src/main/kotlin/dreifa/app/opentelemetry/Propogator.kt
@@ -10,8 +10,13 @@ import io.opentelemetry.context.propagation.TextMapSetter
 
 // the information passed between layers
 class OpenTelemetryContext(val traceId: String, val spanId: String) {
+    private constructor(span: Span) : this(span.spanContext.traceId, span.spanContext.spanId)
+    private constructor(spanContext: SpanContext) : this(spanContext.traceId, spanContext.spanId)
+
     companion object {
         val root = OpenTelemetryContext(TraceId.getInvalid(), Span.getInvalid().toString())
+        fun fromSpan(span : Span) : OpenTelemetryContext = OpenTelemetryContext(span)
+        fun fromSpanContext(spanContext : SpanContext) : OpenTelemetryContext = OpenTelemetryContext(spanContext)
     }
 
     fun isRoot() = this == root
